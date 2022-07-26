386333ad2278b600d34457fd8f36eb4a15527cac
bekranbinay <binayberkan@gmail.com>
2022-07-26 13:42:25 +0300

    .

--- creational/eager-singleton/src/design_patterns/EagerSingleton.java
+++
@@ -1,8 +0,0 @@
-package design_patterns;
-
-public class EagerSingleton {
-    public static void main(String[] args) {
-        EagerInitializedSingleton instance = EagerInitializedSingleton.getInstance();
-    }
-}
-
